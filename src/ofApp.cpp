350698f156e6f6471bfc2ac7cf9458b28b6357e9
Ken Ishii <ishiiken@ishiiken-no-MacBook-Air.local>
2016-06-18 01:32:49 +0900

    initial commit

---
+++ src/ofApp.cpp
@@ -0,0 +1,109 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(0);
+    ofEnableBlendMode(OF_BLENDMODE_ADD);
+    cam.setFov(80);
+    cam.setDistance(600);
+    
+    // ofxVboParticles([max particle number], [particle size]);
+    vboPartciles = new ofxVboParticles(20000, 1000);
+    
+    // set friction (0.0 - 1.0);
+    vboPartciles->friction = 0.001;
+    
+    particlePosition = ofVec3f(0, 0, 0);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    particlePosition.z = sin(ofGetElapsedTimef()) * 400;
+    particlePosition.x  = cos(ofGetElapsedTimef()) * 400;
+    particlePosition.y  = sin(ofGetElapsedTimef() / 2.0) * 100;
+    
+    
+    for (int i = 0; i < 40; i++) {
+        ofVec3f position = particlePosition;
+        ofVec3f velocity = ofVec3f(ofRandom(-1, 1), ofRandom(-1, 1), ofRandom(-1, 1));
+        ofColor color;
+        color.setHsb(ofRandom(10, 0), 100, 220);
+        
+        // add a particle
+        vboPartciles->addParticle(position, velocity, color);
+    }
+    
+    vboPartciles->update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofBackgroundGradient(ofColor(127), ofColor(0), OF_GRADIENT_CIRCULAR);
+    
+    cam.begin();
+    ofRotate(ofGetElapsedTimef() * 10, 0, 1, 1);
+    vboPartciles->draw();
+    cam.end();
+    
+    ofSetColor(255);
+    ofDrawBitmapString(ofToString(ofGetFrameRate(), 4) + "fps", 10, 20);
+    ofDrawBitmapString("[f] key : toggle fullscreen", 10, 50);
+    ofDrawBitmapString("[a] key : less particles", 10, 65);
+    ofDrawBitmapString("[b] key : return", 10, 80);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+    if (key == 'f') {
+        ofToggleFullscreen();
+    }
+    if (key == 'a'){
+        cam.setFov(10);
+        cam.setDistance(10);
+    }
+    if (key == 'b'){
+        cam.setFov(80);
+        cam.setDistance(600);
+    }
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
+}
